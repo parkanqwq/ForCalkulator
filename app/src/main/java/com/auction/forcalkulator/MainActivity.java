653d22173c78d455f565c9cf491d69789010cca1
Maksim Kalabukhov <bfmaximmm1992@gmail.com>
2021-05-11 20:41:35 +0300

    запускник калькулятора

--- app/src/main/java/com/auction/forcalkulator/MainActivity.java
+++ app/src/main/java/com/auction/forcalkulator/MainActivity.java
@@ -1,8 +1,24 @@
 package com.auction.forcalkulator;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.widget.Button;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
-    
+    private Button button;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        button = findViewById(R.id.button);
+        button.setOnClickListener(view -> {
+            Intent intent = new Intent("Calculator");
+            startActivity(intent);
+        });
+    }
 }
